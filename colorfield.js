e699129fa9e75efa41a4aca2cc8237e8bbf89613
Kodie Grantham <kodie.grantham@gmail.com>
2021-09-29 11:08:11 -0500

    Initial Commit

---
+++ colorfield.js
@@ -0,0 +1,131 @@
+/*!
+  colorfield v0.0.1 (https://github.com/kodie/colorfield)
+  by Kodie Grantham (https://kodieg.com)
+*/
+
+var colorfield = function () {
+  var elements = document.getElementsByClassName('colorfield')
+
+  for (var i = 0; i < elements.length; i++) {
+    (function (element) {
+      var input = element.querySelector('input[type="color"]')
+
+      if (input) {
+        var openElements = element.getElementsByClassName('colorfield-open')
+        var valueInputs = element.getElementsByClassName('colorfield-input')
+        var valueElements = element.getElementsByClassName('colorfield-value')
+        var stylePropElements = element.querySelectorAll('[data-colorfield-style-prop]')
+
+        element.open = function () {
+          input.focus()
+          input.click()
+        }
+
+        element.set = function (value) {
+          var currentValue = input.value
+
+          if (value.charAt(0) !== '#') value = '#' + value
+          if (value === currentValue) return
+          if (value.length > 7) value = value.substr(0, 7)
+
+          if (value.length >= 4 && value.length < 7) {
+            value = '#' + value[1] + value[1] + value[2] + value[2] + value[3] + value[3]
+          }
+
+          input.value = value
+
+          colorfield.triggerEvent('change', input)
+        }
+
+        element.get = function () {
+          return input.value
+        }
+
+        if (openElements.length) {
+          for (var j = 0; j < openElements.length; j++) {
+            var openElement = openElements[j]
+
+            openElement.addEventListener('click', function (e) {
+              element.open()
+            }, false)
+
+            openElement.addEventListener('focus', function (e) {
+              element.open()
+            }, false)
+          }
+        }
+
+        if (valueInputs.length) {
+          for (var j = 0; j < valueInputs.length; j++) {
+            var valueInput = valueInputs[j]
+
+            valueInput.addEventListener('change', function (e) {
+              if (!e.colorfieldTriggered) {
+                element.set(valueInput.value)
+              }
+            }, false)
+
+            valueInput.addEventListener('input', function (e) {
+              if (valueInput.colorfieldTimer) {
+                clearTimeout(valueInput.colorfieldTimer)
+              }
+
+              valueInput.colorfieldTimer = colorfield.triggerEvent('change', valueInput, {}, 1000)
+            })
+          }
+        }
+
+        input.addEventListener('change', function (e) {
+          if (valueInputs.length) {
+            for (var j = 0; j < valueInputs.length; j++) {
+              var valueInput = valueInputs[j]
+              valueInput.value = input.value
+            }
+          }
+
+          if (valueElements.length) {
+            for (var j = 0; j < valueElements.length; j++) {
+              var valueElement = valueElements[j]
+              var valueElementType = valueElement.tagName.toLowerCase()
+
+              if (valueElementType === 'input') {
+                valueElement.val = input.value
+              } else {
+                valueElement.textContent = input.value
+              }
+            }
+          }
+
+          if (stylePropElements.length) {
+            for (var j = 0; j < stylePropElements.length; j++) {
+              var stylePropElement = stylePropElements[j]
+              var styleProps = stylePropElement.getAttribute('data-colorfield-style-prop').split(',')
+
+              for (var k = 0; k < styleProps.length; k++) {
+                stylePropElement.style[styleProps[k]] = input.value
+              }
+            }
+          }
+        })
+
+        colorfield.triggerEvent('change', input)
+      } else {
+        console.warn(element, 'An color input field is required for each colorfield instance.')
+      }
+    })(elements[i])
+  }
+
+  return elements
+}
+
+colorfield.triggerEvent = function (type, element, data, delay) {
+  return setTimeout(function () {
+    var e = new Event(type, { bubbles: true })
+
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) e[key] = data[key]
+    }
+
+    element.dispatchEvent(e)
+  }, delay || 1)
+}
---
+++ readme.md
@@ -0,0 +1,85 @@
+# colorfield
+
+A tiny, dependency-free, color input field helper that utilizes the native color picker.
+
+
+## Demo
+Visit https://kodie.github.io/colorfield
+
+
+## Usage
+Add the CSS (which is simply just used to hide the native color input):
+
+```html
+<link rel="stylesheet" href="dist/colorfield.min.css" />
+```
+
+and the JS:
+
+```html
+<script type="text/javascript" src="dist/colorfield.min.js"></script>
+```
+
+and your HTML:
+
+```html
+<div class="colorfield">
+  <input type="color" id="main-color" value="#5185b3">
+  <button type="button" class="colorfield-open colorfield-value" data-colorfield-style-prop="background-color"></button>
+</div>
+```
+
+And then just use the function below:
+
+
+### The `colorfield` Function
+
+Initiates colorfield on any element that has the `colorfield` class. This should be a container around your color input.
+
+
+#### Example
+```js
+window.addEventListener('load', function () {
+  colorfield()
+})
+```
+
+
+## Attributes/Classes
+
+ * The `colorfield-input` class - Any input element with this class will have it's value set to the selected color any time the color is changed. The color will also be set to any value that is entered into this input field.
+
+ * The `colorfield-open` class - Any element with this class will open the color picker when clicked.
+
+ * The `colorfield-value` class - Any element with this class will have it's text content set to the color when the color is changed. If this element is an input, it's value will be updated instead of text content.
+
+ * The `data-colorfield-style-prop` attribute - Set this attribute to a style property (like `background` or `color`) on an element to have it's style updated when the color is changed. Multiple style properties can be defined by separating them with a comma (ie. `background,color`).
+
+
+## JavaScript Methods
+
+The containing colorfield element (the element with the `colorfield` class) will have the following methods attached to it available for use:
+
+```js
+var color = document.querySelector('.color')
+
+color.open() // Opens the color picker
+
+color.set('#00ff00') // Sets the color
+
+var currentColor = color.get() // Gets the current color
+
+// The `change` event bubbles up to the containing element so you can detect changes like so:
+color.addEventListener('change', function () {
+  document.body.style.setProperty('--color', event.target.value)
+})
+```
+
+
+## Related
+
+ - [filebokz](https://github.com/kodie/filebokz) - A tiny, dependency-free, highly customizable and configurable, easy to use file input with some pretty sweet features.
+
+
+## License
+MIT. See the [license file](license.md) for more info.
